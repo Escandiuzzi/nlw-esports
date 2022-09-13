fb02dbdf0bb387cf7c18b858057ec9a3e6b8dc21
Luiz Felipe Escandiuzzi <lfescandiuzzi@icloud.com>
2022-09-12 21:19:17 -0300

    High Speed

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,14 @@
+import './styles/main.css'
+
+import logoImg from './assets/logo.svg';
+
 function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt=""/>
+      <h1 className="text-6xl text-white font-black mt-28">Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.</h1>
+    </div>
+  )
 }
 
 export default App
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/**.tsx',
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        
+      },
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
+      }
+    },
+  },
+  plugins: [],
+}
